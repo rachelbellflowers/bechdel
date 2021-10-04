9966da99dc94edc01d6fda299f965985ac9dac6f
rachelbellflowers <rebellflowers@tutanota.com>
2021-10-03 22:21:15 -0400

    Fixed graphs

--- bechdel.R
+++ bechdel.R
@@ -3,6 +3,8 @@ p_load(data.table, tidyverse, sqldf, DT, reactable, flexdashboard, htmlwidgets, 
 
 # https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-03-09/readme.md
 
+#factors
+
 # Import
 
 raw_bechdel <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/raw_bechdel.csv')
@@ -10,9 +12,10 @@ raw_bechdel <- fread('https://raw.githubusercontent.com/rfordatascience/tidytues
 movies <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv', select = c(1:33))
 
 # Cleaning
-movies <- movies[order(title)]
 
-raw_bechdel <- raw_bechdel[year < 2014][order(title)]
+movies[movies=="N/A"] <- NA
+
+movies <- movies[order(title)]
 
 movies$title <- gsub("&amp;", "&", movies$title)
 
@@ -28,6 +31,8 @@ movies$title <- gsub("&uuml;","ü", movies$title)
 
 ###
 
+raw_bechdel <- raw_bechdel[year < 2014][order(title)]
+
 raw_bechdel$title <- gsub("&amp;", "&", raw_bechdel$title)
 
 raw_bechdel$title <- gsub("&#39;|â€™", "'", raw_bechdel$title)
@@ -36,15 +41,28 @@ raw_bechdel$title <- gsub("&quot;","\"", raw_bechdel$title)
 
 ###
 
-# Join
+# Joins and New Columns
 movbech <- raw_bechdel[movies, on = .(title, year)]
 
 movbech[, budget_2013_mean := mean(budget_2013), by = binary]
 
 # Graphs
 
-ggplot(movbech, aes(budget_2013, binary)) +
- geom_bar(stat="summary", fun = mean)
+# level_order <- c('virginica', 'versicolor', 'setosa') #this vector might be useful for other plots/analyses
 
+# ggplot(iris, aes(x = factor(Species, level = level_order), y = Petal.Width)) + geom_col()
+
+eval_order <- factor(movbech$rated, level = c("G", "PG", "PG-13", "TV-PG", "TV-14", "R", "NC-17", "X", "Not Rated", "Unrated", "NA"))
+
+
+ggplot(movbech, aes(eval_order))+
+  geom_bar() +
+  labs(title="Movie Ratings", x="Rating", y="Count") +
+  theme_minimal()
+
+ggplot(movbech, aes(binary)) +
+  geom_bar() +
+  labs(title="Bechdel Pass Rate", x="Rating", y="Count") +
+  theme_minimal()
 
 
