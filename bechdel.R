83fc71aec4145e36e345376c1007ef8aa74c4ed5
rachelbellflowers <rebellflowers@tutanota.com>
2021-10-03 21:37:03 -0400

    Graph, joining tables

--- bechdel.R
+++ bechdel.R
@@ -1,5 +1,5 @@
 library(pacman)
-p_load(ggplot2, data.table, openxlsx, DT, reactable, flexdashboard, tidyverse)
+p_load(data.table, tidyverse, sqldf, DT, reactable, flexdashboard, htmlwidgets, bslib)
 
 # https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-03-09/readme.md
 
@@ -7,13 +7,26 @@ p_load(ggplot2, data.table, openxlsx, DT, reactable, flexdashboard, tidyverse)
 
 raw_bechdel <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/raw_bechdel.csv')
 
-movies <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv')
+movies <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv', select = c(1:33))
 
 # Cleaning
+movies <- movies[order(title)]
+
+raw_bechdel <- raw_bechdel[year < 2014][order(title)]
 
 movies$title <- gsub("&amp;", "&", movies$title)
 
 movies$title <- gsub("&#39;", "'", movies$title)
+
+movies$title <- gsub("&agrave;","à", movies$title)
+
+movies$title <- gsub("&auml;","ä", movies$title)
+
+movies$title <- gsub("&aring;","å", movies$title)
+
+movies$title <- gsub("&uuml;","ü", movies$title)
+
+###
 
 raw_bechdel$title <- gsub("&amp;", "&", raw_bechdel$title)
 
@@ -21,5 +34,17 @@ raw_bechdel$title <- gsub("&#39;|â€™", "'", raw_bechdel$title)
 
 raw_bechdel$title <- gsub("&quot;","\"", raw_bechdel$title)
 
-movies <- movies[order(title)]
-raw_bechdel <- raw_bechdel[order(title)]
+###
+
+# Join
+movbech <- raw_bechdel[movies, on = .(title, year)]
+
+movbech[, budget_2013_mean := mean(budget_2013), by = binary]
+
+# Graphs
+
+ggplot(movbech, aes(budget_2013, binary)) +
+ geom_bar(stat="summary", fun = mean)
+
+
+
