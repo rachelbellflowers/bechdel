7db5b25cdb6ce8009ffe7eae2185c7cb16269562
rachelbellflowers <rebellflowers@tutanota.com>
2021-10-06 18:01:42 -0400

    No conclusive results from inferential tests

--- bechdel.R
+++ bechdel.R
@@ -1,5 +1,5 @@
 library(pacman)
-p_load(data.table, tidyverse, sqldf, DT, reactable, flexdashboard, htmlwidgets, bslib)
+p_load(data.table, tidyverse, corrplot, sqldf, DT, reactable, flexdashboard, htmlwidgets, bslib)
 
 # https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-03-09/readme.md
 
@@ -52,54 +52,6 @@ movbech[, imdb_votes:=as.integer(imdb_votes)]
 
 
 # New Numerical Columns
-
-## Pass/Fail
-
-movbech$binary_num <- fifelse(movbech$binary=="FAIL", 0, 1)
-
-## Ratings
-
-movbech$G <- fifelse(movbech$rated=="G", 1, 0)
-movbech$PG <- fifelse(movbech$rated=="PG" | movbech$rated=="TV-PG", 1, 0)
-movbech$R <- fifelse(movbech$rated=="R", 1, 0)
-movbech$NC_17 <- fifelse(movbech$rated=="NC-17", 1, 0)
-movbech$X <- fifelse(movbech$rated=="X", 1, 0)
-movbech$Not_Rated <- fifelse(movbech$rated=="Not Rated", 1, 0)
-movbech$Unrated <- fifelse(movbech$rated=="Unrated", 1, 0)
-
-ratings <- movbech[, c(38:45)]
-
-
-## Genres
-
-movbech$action <- fifelse(grepl("Action",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$adventure <- fifelse(grepl("Adventure",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$animation <- fifelse(grepl("Animation",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$comedy <- fifelse(grepl("Comedy",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$horror <- fifelse(grepl("Horror",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$drama <- fifelse(grepl("Drama",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$music <- fifelse(grepl("Music",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$sci_Fi <- fifelse(grepl("Sci/-Fi",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$crime <- fifelse(grepl("Crime",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$romance <- fifelse(grepl("Romance",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$fantasy <- fifelse(grepl("Fantasy",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$thriller <- fifelse(grepl("Thriller",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$family <- fifelse(grepl("Family",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$war <- fifelse(grepl("War",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$history <- fifelse(grepl("History",movbech$genre, ignore.case = TRUE),1, 0)
-movbech$musical <- fifelse(grepl("Musical",movbech$genre, ignore.case = TRUE),1, 0)
-
-
-
-genre <- movbech[,c(38, 46:61)]
-
-cor.test(genre$binary_num, genre$action)
-cor.test(genre$binary_num, genre$romance)
-cor.test(genre$binary_num, genre$crime)
-## 
-
-# Bechdel score relation to country, language
-
 
 # Graphs
 
