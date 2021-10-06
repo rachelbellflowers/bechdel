254b8eeb9c4649d99949d759c1a7dafcfedabb4c
rachelbellflowers <rebellflowers@tutanota.com>
2021-10-05 21:39:41 -0400

    Cleaned data and prepare for analyses

--- bechdel.R
+++ bechdel.R
@@ -15,7 +15,7 @@ movies <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/m
 
 movies[movies=="N/A"] <- NA
 
-movies <- movies[order(title)]
+movies <- movies[!is.na(rated)][order(title)]
 
 movies$title <- gsub("&amp;", "&", movies$title)
 
@@ -46,17 +46,64 @@ movbech <- raw_bechdel[movies, on = .(title, year)][order(title)]
 
 movbech[, budget_2013_mean := mean(budget_2013), by = binary]
 
-movbech$binary_num <- fifelse(movbech$binary=="FAIL", 0, 1)
-
 movbech$imdb_votes <- gsub(",", "", movbech$imdb_votes)
 
 movbech[, imdb_votes:=as.integer(imdb_votes)]
 
-movbech <- movbech[!is.na(rated)]
+
+# New Numerical Columns
+
+## Pass/Fail
+
+movbech$binary_num <- fifelse(movbech$binary=="FAIL", 0, 1)
+
+## Ratings
+
+movbech$G <- fifelse(movbech$rated=="G", 1, 0)
+movbech$PG <- fifelse(movbech$rated=="PG" | movbech$rated=="TV-PG", 1, 0)
+movbech$R <- fifelse(movbech$rated=="R", 1, 0)
+movbech$NC_17 <- fifelse(movbech$rated=="NC-17", 1, 0)
+movbech$X <- fifelse(movbech$rated=="X", 1, 0)
+movbech$Not_Rated <- fifelse(movbech$rated=="Not Rated", 1, 0)
+movbech$Unrated <- fifelse(movbech$rated=="Unrated", 1, 0)
+
+ratings <- movbech[, c(38:45)]
+
+
+## Genres
+
+movbech$action <- fifelse(grepl("Action",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$adventure <- fifelse(grepl("Adventure",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$animation <- fifelse(grepl("Animation",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$comedy <- fifelse(grepl("Comedy",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$horror <- fifelse(grepl("Horror",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$drama <- fifelse(grepl("Drama",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$music <- fifelse(grepl("Music",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$sci_Fi <- fifelse(grepl("Sci/-Fi",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$crime <- fifelse(grepl("Crime",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$romance <- fifelse(grepl("Romance",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$fantasy <- fifelse(grepl("Fantasy",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$thriller <- fifelse(grepl("Thriller",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$family <- fifelse(grepl("Family",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$war <- fifelse(grepl("War",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$history <- fifelse(grepl("History",movbech$genre, ignore.case = TRUE),1, 0)
+movbech$musical <- fifelse(grepl("Musical",movbech$genre, ignore.case = TRUE),1, 0)
+
+
+
+genre <- movbech[,c(38, 46:61)]
+
+cor.test(genre$binary_num, genre$action)
+cor.test(genre$binary_num, genre$romance)
+cor.test(genre$binary_num, genre$crime)
+## 
+
+# Bechdel score relation to country, language
+
 
 # Graphs
 
-eval_order <- factor(movbech$rated, level = c("G", "PG", "PG-13", "TV-PG", "TV-14", "R", "NC-17", "X", "Not Rated", "Unrated", "NA"))
+eval_order <- factor(movbech$rated, level = c("G", "PG", "PG-13", "TV-PG", "TV-14", "R", "NC-17", "X", "Not Rated", "Unrated"))
 
 
 ggplot(movbech, aes(eval_order))+
@@ -71,5 +118,3 @@ ggplot(movbech, aes(budget_2013, domgross_2013)) +
   geom_smooth(color="red", se=FALSE) +
   geom_line()
 
-
-# Bechdel score relation to movie rating, year, genre, movie length
