a52e12833fd926209a1f8960f993fec4049d5840
rachelbellflowers <rebellflowers@tutanota.com>
2021-10-15 22:53:13 -0400

    Finished data prep

--- bechdel.R
+++ bechdel.R
@@ -1,5 +1,5 @@
 library(pacman)
-p_load(data.table, tidyverse, corrplot, sqldf, DT, reactable, flexdashboard, htmlwidgets, bslib)
+p_load(data.table, tidyverse, corrplot, sqldf, DT, reactable, flexdashboard, htmlwidgets, bslib, lubridate)
 
 # https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-03-09/readme.md
 
@@ -50,22 +50,31 @@ movbech$imdb_votes <- gsub(",", "", movbech$imdb_votes)
 
 movbech[, imdb_votes:=as.integer(imdb_votes)]
 
-
+movbech$released <- dmy(movbech$released)
 # New Numerical Columns
 
 # Graphs
 
 eval_order <- factor(movbech$rated, level = c("G", "PG", "PG-13", "TV-PG", "TV-14", "R", "NC-17", "X", "Not Rated", "Unrated"))
 
-ggplot(movbech, aes(year, fill = factor(year))) +
+
+############################
+
+movbech[year %between% c(1970, 1979), decade:="70s"]
+movbech[year %between% c(1980, 1989), decade:="80s"]
+movbech[year %between% c(1990, 1999), decade:="90s"]
+movbech[year %between% c(2000, 2009), decade:="00s"]
+movbech[year %between% c(2010, 2019), decade:="10s"]
+
+ggplot(movbech, aes(year, fill=decade)) +
   geom_bar() +
+  scale_fill_manual(values=c("#CE6693FF", "#A059A0FF", "#FAC484FF", "#F8A07EFF", "#EB7F86FF")) +
   theme_minimal()
 
-
+##################################
 ggplot(movbech, aes(eval_order))+
   geom_bar() +
   labs(title="Movie Ratings", x="Rating", y="Count")
-
 
 ggplot(movbech, aes(budget_2013, domgross_2013)) +
   geom_smooth(color="red", se=FALSE) +
@@ -77,3 +86,5 @@ ggplot(movbech, aes(budget_2013, domgross_2013)) +
 ggplot(movbech, aes(binary)) +
   geom_bar() +
   labs(title="Bechdel Pass Rate", x="Rating", y="Count")
+
+
